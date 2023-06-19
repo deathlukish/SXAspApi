5527a5e44d0e648753d0e55a050dba2d82c35c55
Дмитрий <deathlukish@mail.ru>
2023-06-19 14:17:06 +0300

    UserManager controller

---
+++ SXWebClient/Controllers/UserManagerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SXWebClient.Controllers
+{
+    public class UserManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
